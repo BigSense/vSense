756fe3d9feabb7e3dbe6e8b35827a50fca0f74bf
Sumit Khanna <sumit@catalyst.net.nz>
2015-01-28 14:32:48 +1300

    vsense security action fully implemented with validation

--- lib/secure.rb
+++ lib/secure.rb
@@ -4,12 +4,14 @@ require_relative 'env'
 
 class SecureAction < Action
 
+  GPG_ID_FILE = File.join(Dir.home, '.password-store', '.gpg-id')
+
   def set_options()
     super
     @options = {}
     @opts = OptionParser.new do |opts|
 
-      opts.banner = 'Usage: vsense secure [-s <key_file> ] [-p <gpg key id>]'
+      opts.banner = 'Usage: vsense secure [-s <key_file> ] [-p <gpg key id> [-f]]'
 
       opts.on('-s', '--ssh KEY', 'SSH identity for vagrant user in all new VMs') do |f|
         @options[:ssh_key_file] = f
@@ -17,6 +19,10 @@ class SecureAction < Action
 
       opts.on('-p', '--pgp ID', 'ID of PGP key to use for password encryption') do |id|
         @options[:pgp_id] = id
+      end
+
+      opts.on('-f', '--force', 'Overwrite existing PGP ID in #{GPG_ID_FILE}') do |f|
+        @options[:force_pgp] = f
       end
 
       opts.on_tail("-h", "--help", "Show this message") do
@@ -30,14 +36,21 @@ class SecureAction < Action
 
   def validate()
 
+    # we don't call super because secure doesn't require
+    #  an environment name
+
     if not @options[:pgp_id] and not @options[:ssh_key_file]
-      puts "here"
       STDERR.puts @opts
       exit 1
     end
 
     if @args.length > 0
       STDERR.puts ('Unknown trailing arguments: %s' %[@args.drop(1)]).red
+      exit 1
+    end
+
+    if @options[:force_pgp] and not @options[:pgp_id]
+      STDERR.puts '-f/--force can only be used with -p/--pgp ID'.red
       exit 1
     end
 
@@ -51,6 +64,17 @@ class SecureAction < Action
         STDERR.puts out.red
         exit 1
       end
+
+      if File.exists?(GPG_ID_FILE)
+        lines = File.readlines(GPG_ID_FILE)
+        if lines.length > 0 and lines[0].strip != @options[:pgp_id] and not @options[:force_pgp]
+          STDERR.puts "PGP key ID in #{GPG_ID_FILE} is already set to #{lines[0].strip}.\n" \
+                      "If you change this, you may not be able to access existing passwords.\n" \
+                      "Use -f to force".red #TODO: implement -f
+          exit 1
+        end
+      end
+
     end
 
     if @options[:ssh_key_file] and not File.exists?(@options[:ssh_key_file])
@@ -70,11 +94,14 @@ class SecureAction < Action
     if @options[:pgp_id]
       puts "Setting pgp ID to #{@options[:pgp_id]}".green
       Environment::pgp_id = @options[:pgp_id]
+      File.open(GPG_ID_FILE,'w') do |f|
+        f.write(@options[:pgp_id])
+      end
     end
 
-    puts "\nNote: Security only takes place during provisioning. Existing environments remain unchanged".cyan
-    puts "      It is recommended you run ssh-agent and gpg-agent".cyan
-    puts "      Passwords are stored in ~/.password-store (see http://www.passwordstore.org/⟩".cyan
+    puts "Note: Security only takes place during provisioning. Existing environments remain unchanged".cyan
+    puts "      It is recommended you run ssh-agent and gpg-agent.".cyan
+    puts "      Passwords are stored in #{GPG_ID_FILE} (see http://www.passwordstore.org/⟩".cyan
 
   end
 
